0043c00dc26cfafe1564dc2242f1c0875dba5c38
Luanpadao <thanhluannguyen200101@gmail.com>
2023-03-12 13:20:32 +0700

    update

--- public/js/FC1_Common.js
+++ public/js/FC1_Common.js
@@ -324,6 +324,29 @@ function myTimer() {
     fn_IOFieldDataShow('pos_x','px',0);
     fn_IOFieldDataShow('pos_y','py',0);
     fn_IOFieldDataShow('pos_z','pz',0);
+    fn_SymbolStatus('ss_i1','sstc','ss_i1');
+    fn_SymbolStatus('ss_i2','sstc','ss_i2');
+    fn_SymbolStatus('ss_o','sstc','ss_o');
+    fn_SymbolStatus('dc_i','dc','dc_i');
+    fn_SymbolStatus('dc_o','dc','dc_o');
+    fn_SymbolStatus('n1','','pos_1');
+    fn_SymbolStatus('n2','','pos_2');
+    fn_SymbolStatus('n3','','pos_3');
+    fn_SymbolStatus('n4','','pos_4');
+    fn_SymbolStatus('n5','','pos_5');
+    fn_SymbolStatus('n6','','pos_6');
+    fn_SymbolStatus('n7','','pos_7');
+    fn_SymbolStatus('n8','','pos_8');
+    fn_SymbolStatus('n9','','pos_9');
+    fn_SymbolStatus('n10','','pos_10');
+    fn_SymbolStatus('n11','','pos_11');
+    fn_SymbolStatus('n12','','pos_12');
+    fn_SymbolStatus('n13','','pos_13');
+    fn_SymbolStatus('n14','','pos_14');
+    fn_SymbolStatus('n15','','pos_15');
+    fn_SymbolStatus('n16','','pos_16');
+    fn_SymbolStatus('n17','','pos_17');
+    fn_SymbolStatus('n18','','pos_18');
 }
  
 // Hàm hiển thị dữ liệu lên IO Field
@@ -353,4 +376,36 @@ function fn_ScreenChange(scr_1, scr_2, scr_3, scr_4)
     document.getElementById(scr_2).style.visibility = 'hidden';    // Ẩn trang 1
     document.getElementById(scr_3).style.visibility = 'hidden';    // Ẩn trang 2
     document.getElementById(scr_4).style.visibility = 'hidden';    // Ẩn trang 2
+}
+// Hàm chức năng hiển thị trạng thái symbol
+function fn_SymbolStatus(ObjectID, SymName, Tag)
+{
+    if(ObjectID.substr(0,1) == "n")
+    {
+        socket.on(Tag, function(data){
+            if (data == false)
+            {
+                document.getElementById(ObjectID).classList.add('d-none');
+            }
+            else if (data == true)
+            {
+                document.getElementById(ObjectID).classList.remove('d-none');
+            }
+        });
+    }
+    else
+    {
+        var imglink_0 = "images/Symbol/" + SymName + "_0.png"; // Trạng thái tag = false
+        var imglink_1 = "images/Symbol/" + SymName + "_1.png"; // Trạng thái tag = true
+        socket.on(Tag, function(data){
+            if (data == false)
+            {
+                document.getElementById(ObjectID).src = imglink_0;
+            }
+            else if (data == true)
+            {
+                document.getElementById(ObjectID).src = imglink_1;
+            }
+        });
+    }
 }
